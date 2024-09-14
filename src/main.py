67012f6d93d68f97a131b3165f41ac4845f8c721
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-09-14 20:30:00 +0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,5 @@
+"""
+Magic Prism Package Manager written in Python that helps unify the way utilities
+are installed across platforms. This is an abstraction for different package
+managers so that all commands have a common interface.
+"""
