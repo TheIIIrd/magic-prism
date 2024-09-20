0f7e474260b0acba9c467a0841dba1b6d5bc11f9
TheIIIrd <93443446+TheIIIrd@users.noreply.github.com>
2024-09-20 18:41:40 +0300

    Create install.py

---
+++ src/commands_list/install.py
@@ -0,0 +1,15 @@
+from utils import run_command
+
+
+def install_package(package_name):
+    """Устанавливает указанный пакет с помощью пакетного менеджера pacman."""
+    if not package_name:
+        print("❌ Название пакета не указано.")
+        return
+
+    try:
+        output = run_command(["sudo", "pacman", "-Sy", "--noconfirm", package_name])
+        print(f"🎉 Установка {package_name} завершена успешно!\n{output}")
+
+    except RuntimeError as e:
+        print(f"❌ {e}")
